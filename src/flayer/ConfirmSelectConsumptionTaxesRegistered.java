36fa1547a31ea1afa2285f0bceec57f61be7ce71
WHtheGetHard <nakashima.hi5@gmail.com>
2019-07-06 12:33:26 +0900

    消費税の登録有無の確認メソッドを作成

---
+++ src/flayer/ConfirmSelectConsumptionTaxesRegistered.java
@@ -0,0 +1,52 @@
+package flayer;
+
+import java.sql.SQLException;
+
+import dlayer.SelectConsumptionTaxes;
+
+public class ConfirmSelectConsumptionTaxesRegistered {
+	SelectConsumptionTaxes selectConsumptionTaxes = new SelectConsumptionTaxes();
+
+	public boolean isDuplicatePrimaryKey(String startDay, String endDay) {
+		boolean isDuplicated = false;
+
+		int countNumber = 0;
+
+		try {
+			countNumber = selectConsumptionTaxes.selectCountFromPrimaryKey(startDay, endDay);
+		} catch (SQLException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+
+		if (countNumber == 1) {
+			isDuplicated = true;
+		} else {
+			isDuplicated = false;
+		}
+
+		return isDuplicated;
+	}
+
+
+	public boolean isOverlap(String startDay, String endDay) {
+		boolean isOverlaped = false;
+
+		int countNumber = 0;
+
+		try {
+			countNumber = selectConsumptionTaxes.selectCountTermOverLap(startDay, endDay);
+		} catch (SQLException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+
+		if (countNumber == 1) {
+			isOverlaped = true;
+		} else {
+			isOverlaped = false;
+		}
+
+		return isOverlaped;
+	}
+}
