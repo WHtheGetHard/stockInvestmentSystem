842b6480af0bd111b1107a6d55772f17184cb295
WHtheGetHard <nakashima.hi5@gmail.com>
2019-07-08 10:45:36 +0900

    消費税の登録クラスを追加

---
+++ src/fieldformat/DisplayContentsConsumptionTaxes.java
@@ -0,0 +1,33 @@
+package fieldformat;
+
+public class DisplayContentsConsumptionTaxes {
+	private String comsumptionTax;
+
+	private String startDate;
+
+	private String endDate;
+
+	public String getComsumptionTax() {
+		return comsumptionTax;
+	}
+
+	public void setComsumptionTax(String comsumptionTax) {
+		this.comsumptionTax = comsumptionTax;
+	}
+
+	public String getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(String startDate) {
+		this.startDate = startDate;
+	}
+
+	public String getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(String endDate) {
+		this.endDate = endDate;
+	}
+}
---
+++ src/flayer/RegistConsumptionTaxes.java
@@ -0,0 +1,38 @@
+package flayer;
+
+import java.sql.SQLException;
+
+import dlayer.InsertConsumptionTaxes;
+import fieldformat.ConsumptionTaxes;
+import fieldformat.DisplayContentsConsumptionTaxes;
+
+public class RegistConsumptionTaxes {
+	public void execRegistration(DisplayContentsConsumptionTaxes displayContentsConsumptionTaxes) {
+		ConsumptionTaxes consumptionTaxes = new ConsumptionTaxes();
+
+		consumptionTaxes.setTaxRate(displayContentsConsumptionTaxes.getComsumptionTax());
+		consumptionTaxes.setStartDay(displayContentsConsumptionTaxes.getStartDate());
+		consumptionTaxes.setEndDay(displayContentsConsumptionTaxes.getEndDate());
+
+
+		InsertConsumptionTaxes insertConsumptionTaxes = new InsertConsumptionTaxes();
+		ConfirmConsumptionTaxesRegistered confirmConsumptionTaxesRegistered = new ConfirmConsumptionTaxesRegistered();
+
+		// TODO : confirm term is overlaped
+		boolean isOverlaped = confirmConsumptionTaxesRegistered.isOverlap(
+				displayContentsConsumptionTaxes.getStartDate(), displayContentsConsumptionTaxes.getEndDate());
+
+		// TODO : if term is overlaped
+		if (isOverlaped) {
+			// TODO : decide specification
+		} else {
+			// TODO : else not overlaped, insert to consumption_taxes table
+			try {
+				insertConsumptionTaxes.execInsert(consumptionTaxes);
+			} catch (SQLException e) {
+				// TODO 自動生成された catch ブロック
+				e.printStackTrace();
+			}
+		}
+	}
+}
