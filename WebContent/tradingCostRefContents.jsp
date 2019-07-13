8def58a866a0485d36a0f1f50863c9b1d3a44189
WHtheGetHard <nakashima.hi5@gmail.com>
2019-07-13 12:43:01 +0900

    画面遷移は完了

--- WebContent/tradingCostRefContents.jsp
+++ WebContent/tradingCostRefContents.jsp
@@ -1,12 +1,67 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="UTF-8">
-<title>Insert title here</title>
-</head>
-<body>
 
-</body>
-</html>
+<%@ page import="fieldformat.TradingCostTypeSelectCheckResult" %>
+
+<%
+TradingCostTypeSelectCheckResult tradingCostTypeSelectCheckResult
+	= new TradingCostTypeSelectCheckResult();
+
+tradingCostTypeSelectCheckResult
+	= (TradingCostTypeSelectCheckResult) request.getAttribute("tradingCostTypeSelectCheckResult");
+
+if (tradingCostTypeSelectCheckResult != null) {
+	if (tradingCostTypeSelectCheckResult.isValidValue()) {
+		if ("1".equals(tradingCostTypeSelectCheckResult.getTradingCostType())) {
+%>
+			<h2>売買委託手数料情報の確認</h2>
+
+			<form action="GetCostTypeList" method="post">
+				<input type="hidden" name="tradingCostType" value="1">
+				<input type="submit" value="一覧">
+			</form>
+
+			<form action="SearchCostType" method="post">
+				<input type="hidden" name="tradingCostType" value="1">
+				<input type="submit" value="検索">
+			</form>
+
+<%
+		} else if ("2".equals(tradingCostTypeSelectCheckResult.getTradingCostType())) {
+%>
+			<h2>キャピタルゲイン課税情報の確認</h2>
+
+			<form action="GetCostTypeList" method="post">
+				<input type="hidden" name="tradingCostType" value="2">
+				<input type="submit" value="一覧">
+			</form>
+
+
+			<form action="SearchCostType" method="post">
+				<input type="hidden" name="tradingCostType" value="2">
+				<input type="submit" value="検索">
+			</form>
+<%
+		} else if ("3".equals(tradingCostTypeSelectCheckResult.getTradingCostType())) {
+%>
+			<h2>消費税の確認</h2>
+
+			<form action="GetCostTypeList" method="post">
+				<input type="hidden" name="tradingCostType" value="3">
+				<input type="submit" value="一覧">
+			</form>
+
+
+			<form action="SearchCostType" method="post">
+				<input type="hidden" name="tradingCostType" value="3">
+				<input type="submit" value="検索">
+			</form>
+<%
+		}
+	} else {
+%>
+		<%= tradingCostTypeSelectCheckResult.getErrorMessage()%>
+<%
+	}
+}
+%>
---
+++ src/player/GetCostTypeList.java
@@ -0,0 +1,83 @@
+package player;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import fieldformat.BrokerageCommissions;
+import fieldformat.CapitalGainsTaxes;
+import fieldformat.ConsumptionTaxes;
+import fieldformat.TradingCostTypeSelectCheckResult;
+import flayer.ExecSelectAllCapitalGainsTaxes;
+import flayer.ExecSelectAllConsumptionTaxes;
+import flayer.ExecSelectAllRecordBrokerageCommissions;
+
+/**
+ * Servlet implementation class GetCostTypeList
+ */
+@WebServlet("/GetCostTypeList")
+public class GetCostTypeList extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public GetCostTypeList() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String tradingCostType = request.getParameter("tradingCostType");
+
+		TradingCostTypeSelectCheckResult tradingCostTypeSelectCheckResult = new TradingCostTypeSelectCheckResult();
+		tradingCostTypeSelectCheckResult.setTradingCostType(tradingCostType);
+
+		if ("1".equals(tradingCostType)) {
+			ExecSelectAllRecordBrokerageCommissions execSelectAllRecordBrokerageCommissions= new ExecSelectAllRecordBrokerageCommissions();
+			ArrayList<BrokerageCommissions> brokerageCommissionsList = new ArrayList<BrokerageCommissions>();
+
+			brokerageCommissionsList = execSelectAllRecordBrokerageCommissions.exec();
+
+			request.setAttribute("tradingCostTypeSelectCheckResult", tradingCostTypeSelectCheckResult);
+			request.setAttribute("brokerageCommissionsList", brokerageCommissionsList);
+		} else if ("2".equals(tradingCostType)) {
+			ExecSelectAllCapitalGainsTaxes execSelectAllCapitalGainsTaxes = new ExecSelectAllCapitalGainsTaxes();
+			ArrayList<CapitalGainsTaxes> capitalGainsTaxesList = new ArrayList<CapitalGainsTaxes>();
+
+			capitalGainsTaxesList = execSelectAllCapitalGainsTaxes.exec();
+
+			request.setAttribute("tradingCostTypeSelectCheckResult", tradingCostTypeSelectCheckResult);
+			request.setAttribute("capitalGainsTaxesList", capitalGainsTaxesList);
+		} else if ("3".equals(tradingCostType)) {
+			ExecSelectAllConsumptionTaxes execSelectAllConsumptionTaxes = new ExecSelectAllConsumptionTaxes();
+			ArrayList<ConsumptionTaxes> consumptionTaxesList = new ArrayList<ConsumptionTaxes>();
+
+			consumptionTaxesList = execSelectAllConsumptionTaxes.exec();
+
+			request.setAttribute("tradingCostTypeSelectCheckResult", tradingCostTypeSelectCheckResult);
+			request.setAttribute("consumptionTaxesList", consumptionTaxesList);
+		}
+
+		RequestDispatcher rd = request.getRequestDispatcher("refTradingCost.jsp");
+		rd.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
