90d5f6e0717bdc8af409ecb8a22cf492289a0dcf
WHtheGetHard <nakashima.hi5@gmail.com>
2019-07-26 12:13:38 +0900

    追加

---
+++ src/fieldformat/StockBaseInfo.java
@@ -0,0 +1,33 @@
+package fieldformat;
+
+public class StockBaseInfo {
+	private int id;
+
+	private String securitiesCode;
+
+	private String market;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getSecuritiesCode() {
+		return securitiesCode;
+	}
+
+	public void setSecuritiesCode(String securitiesCode) {
+		this.securitiesCode = securitiesCode;
+	}
+
+	public String getMarket() {
+		return market;
+	}
+
+	public void setMarket(String market) {
+		this.market = market;
+	}
+}
