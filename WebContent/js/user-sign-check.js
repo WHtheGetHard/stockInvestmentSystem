83732ce7556063279ebad1acd03d029177fd3b58
WHtheGetHard <nakashima.hi5@gmail.com>
2019-08-05 13:41:46 +0900

    入力チェックによる、サブミットボタンの活性・非活性チェックを追加

--- WebContent/js/user-sign-check.js
+++ WebContent/js/user-sign-check.js
@@ -2,41 +2,79 @@ $('input[type="text"]').blur(function() {
 	let $blurObject = $(this);
 
 	judgeContentsInput($blurObject);
+
+	checkAllInputed();
 });
 
 function judgeContentsInput($blurObject) {
 	let inputedValue = $blurObject.val();
+	let isInputed = false;
 	let inputedContents = $blurObject.prop('title');
 
 	let name = $blurObject.attr('name');
 
 	if (name === 'user-name') {
-		let nameIsInputed = simpleInputCheck(inputedValue);
-
-		if (nameIsInputed) {
-
-		} else {
-			dispErrorWindow('inputError.jsp?title='+inputedContents);
-		}
+		isInputed = simpleInputCheck(inputedValue);
 
 	} else if (name === 'user-password') {
-		let passwordIsValidInput = checkOnlyHalfNumber(inputedValue);
+		isInputed = checkOnlyHalfNumber(inputedValue);
 
-		if (passwordIsValidInput) {
-
-		} else {
-			dispErrorWindow('inputError.jsp?title='+inputedContents);
-		}
 	} else if (name == 'user-mail') {
-		let mailIsValidInput = isMailFormat(inputedValue);
+		isInputed = isMailFormat(inputedValue);
+	}
 
-		if (mailIsValidInput) {
+	isInvalid(isInputed, inputedContents);
+}
 
-		} else
-			dispErrorWindow('inputError.jsp?title='+inputedContents);
+function isInvalid(isInputed, inputedContents) {
+	if (!isInputed) {
+		dispErrorWindow('inputError.jsp?title='+inputedContents);
 	}
 }
 
 function checkAllInputed() {
+	let $allInputText = $('input[type=text]');
+	let allTextNumber = $allInputText.length;
+	let checkCount = 0;
+	let isAllInputed = false;
 
+	$allInputText.each(function(i, element) {
+		let elementValue = $(element).val();
+		let elemetName = $(element).prop('name');
+
+		if (elemetName === 'user-name') {
+			checkCount = checkCountHandle(checkCount, simpleInputCheck(elementValue));
+
+		} else if (elemetName === 'user-password') {
+			checkCount = checkCountHandle(checkCount, checkOnlyHalfNumber(elementValue));
+
+		} else if (elemetName == 'user-mail') {
+			checkCount = checkCountHandle(checkCount, isMailFormat(elementValue));
+		}
+	});
+
+	if (checkCount == allTextNumber) {
+		isAllInputed = true;
+	} else {
+		isAllInputed = false;
+	}
+
+	if (isAllInputed) {
+		$('input[type="submit"]').prop('disabled',false);
+	} else {
+		$('input[type="submit"]').prop('disabled',true);
+	}
+
+}
+
+function checkCountHandle(checkCount, checkResult) {
+	let isAllInputed = false;
+
+	if (checkResult) {
+		checkCount++;
+	} else {
+		checkCount--;
+	}
+
+	return checkCount;
 }
