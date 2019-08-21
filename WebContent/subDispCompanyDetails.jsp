4beda2843db006f072bf25578823e0636b7ea1ae
WHtheGetHard <nakashima.hi5@gmail.com>
2019-08-22 03:44:33 +0900

    セッションとパラメータの子画面への受け渡しを確認

--- WebContent/subDispCompanyDetails.jsp
+++ WebContent/subDispCompanyDetails.jsp
@@ -1,5 +1,8 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+
+<%@ page import="java.util.ArrayList" %>
+<%@ page import="fieldformat.CompanyDetails" %>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -7,9 +10,21 @@
 <title>企業情報詳細</title>
 </head>
 <body>
-	<%
-		int listIndex = Integer.parseInt(request.getParameter("listIndex"));
-	%>
+
+<%
+	int listIndex = Integer.parseInt(request.getParameter("listIndex"));
+
+	ArrayList<CompanyDetails> companyDetailsList = new ArrayList<CompanyDetails>();
+	companyDetailsList = (ArrayList<CompanyDetails>) session.getAttribute("companyDetailsList");
+
+	if (companyDetailsList != null) {
+		if (companyDetailsList.size() > 0) {
+%>
+
+<%
+		}
+	}
+%>
 
 </body>
 </html>
--- src/fieldformat/CompanyDetails.java
+++ src/fieldformat/CompanyDetails.java
@@ -3,15 +3,17 @@ package fieldformat;
 public class CompanyDetails {
 	private String companyName;
 
-	private String sales;
+	private String amountOfSales;
 
-	public String getSales() {
-		return sales;
-	}
+	private String grossProfit;
 
-	public void setSales(String sales) {
-		this.sales = sales;
-	}
+	private String operatingIncome;
+
+	private String managementProfit;
+
+	private String netIncomeBeforeTax;
+
+	private String netIncome;
 
 	public String getCompanyName() {
 		return companyName;
@@ -19,6 +21,54 @@ public class CompanyDetails {
 
 	public void setCompanyName(String companyName) {
 		this.companyName = companyName;
+	}
+
+	public String getAmountOfSales() {
+		return amountOfSales;
+	}
+
+	public void setAmountOfSales(String amountOfSales) {
+		this.amountOfSales = amountOfSales;
+	}
+
+	public String getGrossProfit() {
+		return grossProfit;
+	}
+
+	public void setGrossProfit(String grossProfit) {
+		this.grossProfit = grossProfit;
+	}
+
+	public String getOperatingIncome() {
+		return operatingIncome;
+	}
+
+	public void setOperatingIncome(String operatingIncome) {
+		this.operatingIncome = operatingIncome;
+	}
+
+	public String getManagementProfit() {
+		return managementProfit;
+	}
+
+	public void setManagementProfit(String managementProfit) {
+		this.managementProfit = managementProfit;
+	}
+
+	public String getNetIncomeBeforeTax() {
+		return netIncomeBeforeTax;
+	}
+
+	public void setNetIncomeBeforeTax(String netIncomeBeforeTax) {
+		this.netIncomeBeforeTax = netIncomeBeforeTax;
+	}
+
+	public String getNetIncome() {
+		return netIncome;
+	}
+
+	public void setNetIncome(String netIncome) {
+		this.netIncome = netIncome;
 	}
 
 
